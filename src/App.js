8e66b2820f5a2f0d051093e80dac7afde0267226
natan-arch <natan@bluemond.com.br>
2026-07-26 14:12:57 -0300

    fix: implement solution for item #6

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import Navbar from "../src/Components/Navbar"
+import Navbar from "./Components/Navbar"
 import Story from "../src/Components/Story"
 import SideBox from "./Components/SideBox"
 import Post from "./Components/Posts"
-const App = ()=>{
-    return(
-        <>
-        <Navbar/>
-        <div className="main_container">
+
+const MOCK_POSTS = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 },
+  { id: 7 },
+  { id: 8 },
+]
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="main_container">
         <div className="page_content">
-        <div className="main_content">
-         <Story/>
-         <Post/>
-         <Post/>
-         <Post/>
-         <Post/>
-         <Post/>
-         <Post/>
-         <Post/>
-         <Post/>
+          <div className="main_content">
+            <Story />
+            {MOCK_POSTS.map((post) => (
+              <Post key={post.id} />
+            ))}
+          </div>
+          <SideBox />
         </div>
-        <SideBox/>
-        </div>
-        </div>
-        </>
-    )
-   
+      </div>
+    </>
+  )
 }
 
 export default App;
